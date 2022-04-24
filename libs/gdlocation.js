6529d473796c9c120c41de8ec04681a972041da2
zhousning <244305150@qq.com>
2022-04-24 22:59:41 +0800

    修改轨迹问题

--- libs/gdlocation.js
+++ libs/gdlocation.js
@@ -37,22 +37,20 @@ var gdlocation = {
                     var currentTime = new Date().getTime();
                     var oldLocation = wx.getStorageSync('oldLocation');
                     var oldTime = wx.getStorageSync('oldTime');
-                    var newLocation = data.latitude + "," + data.longitude;
-                    var accuracy = data.accuracy;
+                    var newLocation = data.longitude.toFixed(5) + "," + data.latitude.toFixed(5);
+                    //var accuracy = data.accuracy;
                     //按总共有200个终端，日调用量总30000
                     //3s产生一个点
-                    console.log(newLocation)
                     if (oldLocation != newLocation && currentTime - oldTime > 3000) {
                         wx.setStorageSync('oldLocation', newLocation);
                         wx.setStorageSync('oldTime', currentTime);
                         var points = wx.getStorageSync('points') || [];
                         var point = {
                             location: newLocation,
-                            locatetime: currentTime,
-                            accuracy: accuracy
+                            locatetime: currentTime
                         }
                         points.unshift(point);
-                        console.log(points);
+                        console.log(points.length);
                         wx.setStorageSync('points', points)
                         //30s上传一次，一次上传不超过10个点
                         if (points.length > 10) {
@@ -62,6 +60,7 @@ var gdlocation = {
                 });
             },
             fail: (err) => {
+                console.log(err)
                 wx.openSetting()
             }
         })
